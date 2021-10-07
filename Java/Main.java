8b7300374699e6b2f7892b31bcaa47fa84c97888
EGchoi-dev <egchoi.dev@gmail.com>
2021-10-07 22:20:38 +0900

    lecture05

--- Java/Main.java
+++ Java/Main.java
@@ -2,6 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         //new lecture02.Greedy();
-        new lecture04.Reorder();
+        //new lecture04.Reorder();
+        new lecture05.Q02();
+        new lecture05.Q03();
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,10 @@
 import lecture02
 import lecture04
+import lecture05
 
 #lecture02.greedy();
-lecture04.reorder();
+#lecture04.reorder();
+lecture05.q01();
+lecture05.q02();
+lecture05.q03();
+lecture05.q04();
